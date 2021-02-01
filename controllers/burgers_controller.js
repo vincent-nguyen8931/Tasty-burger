6bbe1239131b4a576f78569e42d80a704e1751ba
Vincent Nguyen <vincent.nguyen8931@gmail.com>
2021-02-01 01:44:40 -0800

    Create index.handlebars and fille with starting code.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -27,7 +27,7 @@ router.post("/api/burger", function(req, res) {
   });
 });
 
-router.put("/api/cats/:id", function(req, res) {
+router.put("/api/burger/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
