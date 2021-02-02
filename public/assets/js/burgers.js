72128451d505419a75273e68f483c3fd2fe3e624
Vincent Nguyen <vincent.nguyen8931@gmail.com>
2021-02-02 03:17:33 -0800

    Burgers move to their proper position when eaten or ordering another upon creation.

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -27,7 +27,7 @@ $(function() {
 
     var newBurger = {
       burger_name: $("#ca").val().trim(),
-      devoured: $("[name=devour]:checked").val().trim()
+      devoured: $("[name=devoured]:checked").val().trim()
     };
 
     // Send the POST request.
