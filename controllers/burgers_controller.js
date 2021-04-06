4bad97c6388ea1497669431cff439b0d8540b612
Vincent Nguyen <vincent.nguyen8931@gmail.com>
2021-04-06 02:53:21 -0700

    Update comment in burgers_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -22,14 +22,13 @@ router.post("/api/burgers", function(req, res) {
   ], [
     req.body.burger_name, req.body.devoured
   ], function(result) {
-    // Send back the ID of the new quote
+    // Send back the ID of the new burger
     res.json({ id: result.insertId });
   });
 });
 
 router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
-
   console.log("condition", condition);
 
   burger.updateOne({
